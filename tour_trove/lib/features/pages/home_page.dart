8902a9170a8b096d97217f8db74a06613f0c9b85
Matheuscamp29 <mdocampello2909@gmail.com>
2025-08-28 18:44:45 -0300

    Botao identificar exposição

--- tour_trove/lib/features/pages/home_page.dart
+++ tour_trove/lib/features/pages/home_page.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:animated_button/animated_button.dart';
+
+//funcao chamada quando botao identificar for clicado 
+void identificar_expsoicao() {
+
+
+
+}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      alignment: Alignment.center,
-      child: const Text("Página principal"),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.green,
+            alignment: Alignment.center,
+            child: const Text("Página principal"),
+          ),
+
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 120, //altura do botao na tela
+            child: Center(
+              child: AnimatedButton(
+                borderRadius: 25, 
+                color: Colors.blue,
+                shadowDegree: ShadowDegree.light,
+                onPressed: identificar_expsoicao,
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Text(
+                    'Identificar',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
